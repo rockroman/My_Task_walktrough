9e1026bb9a7f859b66dc27e64d692d66173d7d05
rockroman <rock.rakic@gmail.com>
2022-11-18 21:18:24 +0000

    fix typo in init.py file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -26,7 +26,7 @@ if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("posgres://"):
+    if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
